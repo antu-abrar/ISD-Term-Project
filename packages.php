f52a5b9ba53a64e44a31b6b126e0909c764653e8
TasfiaZahin <ztasfia25@gmail.com>
2017-06-09 08:28:09 +0600

    packages completed for addition in laravel

---
+++ packages.php
@@ -0,0 +1,111 @@
+<html>
+	<head>
+		<title> <?php echo "EVENT HIVE | Goldwater Convention Center" ?> </title>
+		<base href = "packages.php" />
+		<link href = "style.css" rel = "stylesheet" />
+	</head>
+	
+	<body>
+		<header> 
+			<div class = "container">
+				<img src = "img/event_hive3.png" > </img>
+			</div>
+			<div class = "logout">
+				Home | Logged in as Mumina | Logout
+			</div>
+		</header>
+		
+		<header style = "height:40px; background: linear-gradient(#C0C0C0, #FFFFFF, #C0C0C0);word-spacing: 100px;" > 
+			<input type = "submit" value = "Information">
+			<input type = "submit" value = "Booking">
+			<input type = "submit" value = "Packages">
+			<input type = "submit" value = "Reviews">
+			<input type = "submit" value = "Contact us">
+		</header>
+		
+		<main>
+			<div class = "gallery">
+				<img src = "img/venue1.jpg" > </img>
+				<img src = "img/venue2.jpg" > </img>
+				<img src = "img/venue3.jpg" > </img>
+				<img src = "img/venue4.jpg" > </img>
+			</div>
+			<br>
+			<img src = "img/center_logo.png" > </img>
+			
+			<div class="wrap">
+			<div class="table">
+			<ul style="list-style-type: none";>
+			  
+			<li>
+			  <div class="top">
+				<h2>Package 1</h2>
+				<div class="rectangle">BDT. 119999</div>
+			  </div>
+			  <div class="bottom">
+				<p>Venue</p>
+				<p>Stage Decorations</p>
+				<br><br><br>
+				<div class="book">
+				<input type = "submit" value = "Book">
+				</div>
+			  </div>
+			</li>
+			  
+			<li>
+			  <div class="top">
+				<h2>Package 2</h2>
+				<div class="rectangle">BDT. 139999</div>
+			  </div>
+				<div class="bottom">
+				<p>Venue</p>
+				<p>Stage Decorations</p>
+				<p>Wall Decorations</p>
+				<br><br>
+				<div class="book">
+				<input type = "submit" value = "Book">
+				</div>
+			  </div>
+			</li>
+			  
+			<li>
+			  <div class="top">
+				<h2>Package 3</h2>
+				<div class="rectangle">BDT. 149999</div>
+			  </div>
+				<div class="bottom">
+				<p>Venue</p>
+				<p>Stage Decorations</p>
+				<p>Wall Decorations</p>
+				<p>Ceiling Decorations</p>
+				<br>
+				<div class="book">
+				<input type = "submit" value = "Book">
+				</div>
+			  </div>
+			</li>
+			  
+			<li>
+			  <div class="top">
+				<h2>Package 4</h2>
+				<div class="rectangle">BDT. 199999</div>
+			  </div>
+				<div class="bottom">
+				<p>Venue</p>
+				<p>Stage Decorations</p>
+				<p>Wall Decorations</p>
+				<p>Ceiling Decorations</p>
+				<p>Catering</p>
+				<div class="book">
+				<input type = "submit" value = "Book">
+				</div>
+			  </div>
+			</li>
+			  
+			</ul>
+			</div>
+			</div>
+		</main>
+	</body>
+</html>
+
